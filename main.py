9ab11d5b74ba4692210c760ef80b2f0490bc5def
javi945 <probarcualquiercosa@gmail.com>
2025-07-30 21:26:20 -0500

    stoptword se gurado en disco local c

---
+++ main.py
@@ -0,0 +1,14 @@
+# importar la libreria nltk
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+#definir la ruta donde se descargaarn del nktk
+ntlk.data.path = 'C:/Users/javierç/AppData/Roaming/nltk_data'
+
+
+
+
+
+
--- stopword.py
+++ stopword.py
@@ -9,3 +9,16 @@ lista_stopwords = stopwords.words('spanish')
 
  # imprimir la lista de stopwords
 print(lista_stopwords)
+
+Lista_stopwords_english = stopwords.words('english')
+print(Lista_stopwords_english)
+print(len(Lista_stopwords_english))
+
+
+
+
+
+
+
+
+
