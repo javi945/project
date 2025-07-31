012efcbab0207b02fefa52b3cc4c8b3636b40642
javi945 <probarcualquiercosa@gmail.com>
2025-07-30 19:39:59 -0500

    se importo la libreria nltk, conocimos stopword en diferntes idiomas

---
+++ stopword.py
@@ -0,0 +1,11 @@
+ # importar la libreria nltk
+import nltk
+
+ # desde nltk descargar el paquete stopwords
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+lista_stopwords = stopwords.words('spanish')
+
+ # imprimir la lista de stopwords
+print(lista_stopwords)
